0c4b7a3e563bd244c006528f9c5a467041e9c829
Admazzola <admazzola@gmail.com>
2023-03-21 11:49:38 -0400

    can run test ts

--- degentx-contract/deploy/deploy.ts
+++ degentx-contract/deploy/deploy.ts
@@ -5,40 +5,22 @@ import { deploy } from '../helpers/deploy-helpers'
   
 
 const deployFn: DeployFunction = async (hre:any) => {
-
+  const {  run, network } = hre
    
    
-  let deployer_key =  process.env.PRIVATE_KEY!
 
-
-  const jpegsNft = await deploy<any>({
-    from: deployer_key,
-    contract: 'JpegsNFT',
+  const payspecDeploy = await deploy({
+    contract: 'DegenTx',
     args: [ ],
-    
-    skipIfAlreadyDeployed: false,
-    hre,
-  } )
-
-
-  const tokenAddress = "0xb6ed7644c69416d67b522e20bc294a9a9b405b31";
-  const nftAddress = jpegsNft.address  
-
-  const auction = await deploy<any>({
-    from:deployer_key,
-    contract: 'MemesAuction',
-    args: [ nftAddress, tokenAddress ],
-    
     skipIfAlreadyDeployed: false,
     hre, 
   })
- 
 
 
 }
 
 // tags and deployment
-deployFn.tags = ['auction']
+deployFn.tags = ['degentx']
 deployFn.dependencies = [
   
 ]
